c97ae88ee827711235a8ad5199d0003d95a3f25a
Khor Zhen Win <78037711+khorzhenwin@users.noreply.github.com>
2023-05-05 16:45:22 +0800

    Initial Commit

---
+++ stores/index.jsx
@@ -0,0 +1,1 @@
+export { usePremiumUserStore } from "./usePremiumUserStore";
---
+++ stores/usePremiumUserStore.jsx
@@ -0,0 +1,6 @@
+import { create } from "zustand";
+
+export const usePremiumUserStore = create((set) => ({
+  isPremiumUser: false,
+  setIsPremiumUser: (isPremiumUser) => set({ isPremiumUser: isPremiumUser }),
+}));

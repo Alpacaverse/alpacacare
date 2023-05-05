d630d779dc21eb13052fd4a77a8adb61a0fafa91
Khor Zhen Win <78037711+khorzhenwin@users.noreply.github.com>
2023-05-05 21:41:06 +0800

    [Update] completed landing page

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,16 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Header from "../components/Header";
 import Landing from "../components/Landing";
+import AboutUs from "../components/AboutUs";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Landing />
+      <AboutUs />
+      <Footer />
     </>
   );
 }

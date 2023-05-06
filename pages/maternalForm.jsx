d143465cf7517d3694866f6f8917cbe1d0e30d63
jolinroulin <jolinroulin5805@gmail.com>
2023-05-06 19:15:47 +0800

    maternalForm

---
+++ pages/maternalForm.jsx
@@ -0,0 +1,154 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import PremiumModal from "../components/modal/PremiumModal";
+import {
+  Form,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Grid,
+  Text,
+  Select,
+  Center,
+  Box,
+} from "@chakra-ui/react";
+import { useState, useRef, useEffect } from "react";
+import { MdOutlineUploadFile } from "react-icons/md";
+import {
+  useMaternalStore,
+  usePremiumUserStore,
+  useModelOutputStore,
+} from "../stores";
+const apiHelper = require("../lib/apiHelper");
+
+export default function MaternalForm() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [trialUsed, setTrialUsed] = usePremiumUserStore((state) => [
+    state.trialUsed,
+    state.setTrialUsed,
+  ]);
+  const [diaData, setDiaData] = useModelOutputStore((state) => [
+    state.diaData,
+    state.setDiaData,
+  ]);
+  const [inputFields, setMaternalData, maternalInput, maternalData] =
+    useMaternalStore((state) => [
+      state.inputFields,
+      state.setMaternalData,
+      state.maternalInput,
+      state.maternalData,
+    ]);
+
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  async function handleSubmit() {
+    if (trialUsed) {
+      setIsModalOpen(true);
+      return;
+    } else {
+      setTrialUsed(true);
+    }
+    const data = {};
+    // foreach id of inputFields.label == element id, get value and return
+    inputFields.forEach((element) => {
+      const val = document.getElementById(element.id).value;
+      data[element.id] = val;
+    });
+
+    setMaternalData(data);
+    setIsLoading(true);
+  }
+
+  useEffect(() => {
+    if (maternalInput) {
+      for (const key in maternalInput) {
+        document.getElementById(key).value = maternalInput[key];
+      }
+    }
+  }, [maternalInput]);
+
+  useEffect(() => {
+    if (maternalData) {
+      apiHelper
+        .gestationalAgePrediction(maternalData)
+        .then((res) => setDiaData(res.prediction));
+    }
+  }, [setDiaData]);
+
+  useEffect(() => {
+    if (diaData) {
+      setIsLoading(false);
+      window.location.href = "/report";
+    }
+  }, [diaData]);
+
+  return (
+    <>
+      <Header />
+      <PremiumModal
+        isOpen={isModalOpen}
+        isClose={() => setIsModalOpen(false)}
+      />
+      <Box p={15}>
+        <Center>
+          <Grid
+            templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
+            gap={6}
+            my={4}
+            w={"90%"}
+          >
+            <Text fontWeight={"semibold"} fontSize={"5xl"} color={"orange.400"}>
+              Maternal Assessment{" "}
+            </Text>
+            <Text></Text>
+            {inputFields.map((field) => {
+              return field.options ? (
+                <FormControl key={field.id} id={field.id}>
+                  <FormLabel>{field.label}</FormLabel>
+                  <Select>
+                    {field.options.map((option) => (
+                      <option key={option} value={option}>
+                        {option}
+                      </option>
+                    ))}
+                  </Select>
+                </FormControl>
+              ) : (
+                <FormControl key={field.id} id={field.id}>
+                  <FormLabel>{field.label}</FormLabel>
+                  <Input type="number" step="0.1" />
+                </FormControl>
+              );
+            })}
+          </Grid>
+        </Center>
+        <Center my={6}>
+          <Button
+            colorScheme={"orange"}
+            bg={"orange.400"}
+            type="submit"
+            width={"40%"}
+            justifySelf={"end"}
+            px={6}
+            _hover={{
+              bg: "orange.300",
+            }}
+            onClick={handleSubmit}
+            isLoading={isLoading}
+          >
+            Submit
+          </Button>
+        </Center>
+      </Box>
+      <Box position="fixed" bottom="0" width="100%">
+        <Footer />
+      </Box>
+    </>
+  );
+}
--- stores/index.jsx
+++ stores/index.jsx
@@ -1,3 +1,4 @@
 export { usePremiumUserStore } from "./usePremiumUserStore";
 export { useReportStore } from "./useReportStore";
 export { useModelOutputStore } from "./useModelOutputStore";
+export { useMaternalStore } from "./useMaternalStore";
